0129583f147dfc2dad9e76bf9db6861fbf37b366
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-11-09 21:29:58 +0700

    refactor: combine backend and frontend

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const key = require('./config/main');
+const ConnectDB = require('./config/db');
+const router = require('./routes/main')
+
+const { port, mongoURL } = key;
+
+const app = express();
+
+ConnectDB(mongoURL);
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api', router)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- src/interface/card.d.ts
+++
@@ -1,7 +0,0 @@
-export type ICard = {
-  id: string;
-  type: string;
-  loading: boolean;
-  icon: string;
-  data: string;
-};
