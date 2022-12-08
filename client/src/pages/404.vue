5013eeee3cba9c4fc8c6ef7d31d2d1f533b41306
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-12-08 14:25:05 +0700

    feat: handle map-area coordination

--- client/src/pages/404.vue
+++ client/src/pages/404.vue
@@ -1,7 +1,14 @@
 <template>
- <div class='relative w-full h-full flex flex-row justify-center'>
-    <div class='flex flex-col justify-center'>
-      <div class='flex justify-center text-6xl font-bold text-primary'>4😥4</div>
-    </div>
- </div>
+  <div class="w-screen h-screen overflow-hidden">
+    <img
+      src="/404.jpg"
+      alt="notfound_img"
+      class="bg-cover w-full"
+      usemap="#workmap"
+    />
+
+    <map name="workmap">
+      <area shape="rect" coords="590,610,810,670" alt="button" href="/" />
+    </map>
+  </div>
 </template>
